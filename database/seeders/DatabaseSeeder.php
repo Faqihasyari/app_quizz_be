4deb6259bf02eba9f9f9590ef4000eeb8a3d003f
Faqihasyari <faqih8158@gmail.com>
2025-10-04 21:35:31 +0700

    add answerSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             QuestionSeeder::class,
+            AnswerSeeder::class, // ✅ tambahkan ini
         ]);
     }
 }
